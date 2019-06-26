41bca7db502f80ce6a00d74b94a8a1df0f0133f1
BIT Student <classroom@bgit.rs>
2019-06-26 17:56:44 +0200

    end of class

--- src/app/pages/feed/Feed.js
+++ src/app/pages/feed/Feed.js
@@ -19,6 +19,10 @@ export class Feed extends React.Component {
             })
     }
 
+    showComments = () => {
+
+    }
+
     componentDidMount() {
         this.loadPosts()
 
--- src/app/pages/feed/postItems/VideoPost.js
+++ src/app/pages/feed/postItems/VideoPost.js
@@ -1,20 +1,24 @@
 import React from 'react'
-// import './VideoPost.css'
+// import Link from 'react-router-dom';
 
 export const VideoPost = (props) => {
-    const { post } = props
+    const { post } = props;
+
     return (
-        <div className="videoPost-player embed-responsive embed-responsive-16by9">
-            <iframe id="existing-iframe-example"
-                className="embed-responsive-item"
-                allowfullscreen
-                width="500" height="800"
-                src={`${post.videoUrl}`}
-                frameBorder="0"
-            ></iframe>
-            <div className="videoPost-comments">
-                <p>Comments:{post.comments.length}</p>
+        <>
+            <div className="videoPost-player embed-responsive embed-responsive-16by9">
+                <iframe id="existing-iframe-example"
+                    className="embed-responsive-item"
+                    width="500" height="800"
+                    src={`${post.videoUrl}`}
+                    frameBorder="0"
+                ></iframe>
             </div>
-        </div>
+            <div className="videoPost-comments">
+                <a href="#" onClick={this.showPosts}>Comments: {post.comments.length}</a>
+            </div>
+        </>
     )
 }
+
+// {{console.log(post)}}
